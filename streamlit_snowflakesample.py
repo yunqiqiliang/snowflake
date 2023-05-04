331c72fa7dc0c180c48df680903134a260c712a4
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-05-04 18:31:24 +0800

    Update streamlit_snowflakesample.py

--- streamlit_snowflakesample.py
+++ streamlit_snowflakesample.py
@@ -1,1 +1,29 @@
+# streamlit_app.py
 
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+
+"""
+# Welcome to Streamlit Snowflake Sample!
+"""
+
+"""
+# Initialize connection.
+conn = st.experimental_connection('snowpark')
+
+# Load the table as a dataframe using the Snowpark Session.
+@st.cache_data
+def load_table():
+    with conn.safe_session() as session:
+        return session.table('AMPLITUDE_EVENT').to_pandas()
+
+df = load_table()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.NAME} has a :{row.PET}:")
+
+"""
