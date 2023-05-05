913e2476f1278db6075f9390402cfe9312f175ed
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2023-05-05 11:53:47 +0800

    Update streamlit_snowflakesample.py

--- streamlit_snowflakesample.py
+++ streamlit_snowflakesample.py
@@ -14,12 +14,16 @@ import streamlit as st
 import streamlit as st
 
 # Initialize connection.
-conn = st.experimental_connection('snowflake', type='sql')
+conn = st.experimental_connection('snowpark')
 
-# Perform query.
-df = conn.query('SELECT * from AMPLITUDE_EVENT;', ttl=600)
+# Load the table as a dataframe using the Snowpark Session.
+@st.cache_data
+def load_table():
+    with conn.safe_session() as session:
+        return session.table('AMPLITUDE_EVENT').to_pandas()
+
+df = load_table()
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
-
+    st.write(f"{row.NAME} has a :{row.PET}:")
